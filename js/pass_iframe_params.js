9012fc9d9816929c6a99bbc3957182982638102c
Derek Eder <derek.eder@gmail.com>
2014-04-15 17:01:30 -0500

    fiddling with iframe params

--- js/pass_iframe_params.js
+++ js/pass_iframe_params.js
@@ -1,8 +1,8 @@
 $(document).ready(function()  
 {  
     var loc = window.location.toString(),  
-    params = loc.split('?')[1],  
+    params = loc.split('#/?')[1],  
     iframe = document.getElementById("trackFrame");  
   
-iframe.src = iframe.src + '#/?' + params;  
+iframe.src = 'http://datamade.github.io/fatalities-map/#/?' + params;  
 }); 
